a3456f3cc24910bb0be49c614cb4192a8dceff43
ankitmlive <ankitmlive@gmail.com>
2022-02-14 22:58:41 +0530

    initial structure for python linked list

--- collections/linked_list.py
+++ collections/linked_list.py
@@ -1,12 +1,34 @@
 # A Python file to demonstrate Linked-List in Python
 
 class Element(object):
-	"""A element class for elements in plinked list """
+	"""A element class for elements in linked list """
 	def __init__(self, value):
-		super(Element, self).__init__()
 		self.next = None
 		self.value = value
 
 class LinkedList(object):
-	def __init__(self, element) -> None:
+	def __init__(self, element):
 		self.element = None
+
+	def append(self, new_element):
+		""" function to append element in linked list """
+		current = self.element
+		if self.element:
+			while current.next:
+				current = current.next
+			current.next = new_element
+		else:
+			self.element = new_element
+	
+	def get_position(self, position):
+		"""Get an element from a particular position.
+		Assume the first position is "1".
+		Return "None" if position is not in the list."""
+
+		if self.element and position:
+			pass
+
+
+if __name__ == '__main__':
+	""" main function to execute test case """
+	pass
