d01da61274c950fb4a3aaee0c4134ba4b86ea7db
ankitmlive <ankitmlive@gmail.com>
2022-03-02 23:40:44 +0530

    linked list completion

--- collections/linked_list.py
+++ collections/linked_list.py
@@ -43,12 +43,40 @@ class LinkedList(object):
 		
 		# return None if not any conditon met
 		return None
+	
+	def insert(self, new_element, position):
+		"""Insert a new node at the given position.
+		Assume the first position is "1".
+		Inserting at position 3 means between
+		the 2nd and 3rd elements."""
+		
+		counter = 1
+		current = self.head
+
+		if position > 1:
+			while current and counter < position:
+				if counter == position - 1:
+					new_element.next = current.next
+					current.next = new_element
+				current = current.next
+				counter += 1
+		elif position == 1:
+			new_element.next = self.head
+			self.head = new_element
+		
+	def delete(self, value):
+		"""Delete the first node with a given value."""
+		
+		counter = 1
+		current = self.head
+
+		pass
 
 	def print_list(self):
 		temp = self.head
 		while(temp):
-			print(temp.value)
-			temp = temp.next 
+			print(temp.value, end=" ")
+			temp = temp.next
 
 
 if __name__ == '__main__':
@@ -71,11 +99,28 @@ if __name__ == '__main__':
 	# print all item in linked list
 	print("Printing all values in linked list:")
 	ll.print_list()
+	print("\n")
 
 	# get position of an element
 	print("Value at 1st Position", ll.get_position(1).value)
 	print("Value at 2st Position", ll.get_position(2).value)
 	print("Value at 4rth Position", ll.get_position(4).value)
+
+	# insert a new element on postiton 3 means (between 2 and 3)
+	e5 = Element(5) 
+	ll.insert(e5, 3)
+
+	# now again print all item in linked list to get new added list
+	ll.print_list()
+
+	# Test delete
+	ll.delete(1)
+	# Should print 2 now
+	print(ll.get_position(1).value)
+	# Should print 4 now
+	print(ll.get_position(2).value)
+	# Should print 3 now
+	print: ll.get_position(3).value
 	
 	
 
