0bf1088e36c4d4a3ff1c7564864bc0073b6650d3
ankitmlive <ankitmlive@gmail.com>
2022-03-10 23:38:29 +0530

    linear search algo

---
+++ searching/linear.py
@@ -0,0 +1,14 @@
+# python programmer for linear search 
+
+def linear_search(arr, num):
+    n=len(arr)
+    for i in range(n):
+        if arr[i]==num:
+            return i
+    return -1
+
+if __name__=="__main__":
+    arr=[29, 56, 78, 64, 23, 87, 69, 40]
+    num=23
+    position=linear_search(arr, num)
+    print(f"Position of {num} in given array is {position}")
