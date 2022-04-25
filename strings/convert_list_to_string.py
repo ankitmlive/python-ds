f451efaf3c95f1059dd43cdad3a74b9b4188841c
ankitmlive <ankitmlive@gmail.com>
2022-04-25 10:43:35 +0530

    Completed string from list

--- strings/convert_list_to_string.py
+++ strings/convert_list_to_string.py
@@ -7,5 +7,11 @@ def listToString(input_list):
 
     for list_element in input_list:
         new_str += list_element
-    
+
     return new_str
+
+
+if __name__ == "__main__":
+    input_list = ["My", "Name", "Is", "Ankit"]
+    result = listToString(input_list)
+    print("The Output String is:", result)
